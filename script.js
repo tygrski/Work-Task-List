cac1f2a915be8cef151981c1091974d072881148
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-08 14:36:38 -0500

    collum set, text area, button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const date = new Date();
+const date = moment().format("MMM Do YYYY");
+console.log(date);
 // display current date
 dateDisplayEl = document.getElementById("currentDay").innerHTML = date;
 
-var tasks = {}
+// eval hour and check box is past present or future
 
-// var createTask = function(taskText, taskDate, taskList)
+// when text area input is set, click save button and save to local storage
+
+// on button click time block 9
+// localstorage.setItem(time block id, text value of the textarea box)
+// to retrieve ---> set the text area text to localstorage.getItem(time block id, text value)
+
+// change colors according to time 
