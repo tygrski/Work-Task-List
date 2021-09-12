9c90107230a4ea96deee34f1609c53f112dee164
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-12 13:24:12 -0500

    saveBTN

--- script.js
+++ script.js
@@ -3,35 +3,31 @@ var currentTime = moment().format('MMM Do h:mm A');
 $('#currentDay').text(currentTime);
 
 // eval hour and check box is past present or future
-var past
-var current 
-var future
-var taskHour
-console.log(moment().format(''))
-console.log($(".hour col-lg-2 col-sm-12"))
 $(".row").each(function() {
-   var divHour = parseInt($(this).attr("value"))
-   // console.log(divHour)
+   var divHour = parseInt($(this).attr("value"));
    
    if(moment().format("H") < divHour) {
       $(this).addClass("past");
       
-   }else if (moment().format("H") == divHour){
+   } else if(moment().format("H") == divHour){
       $(this).addClass("present");
       
-   } else if (moment().format("H") > divHour) { 
+   } else if(moment().format("H") > divHour) { 
       $(this).addClass("future");
    }
-   console.log(moment().format("H"))
 });
 
-
-// // click save button and save to local storage
-// $( "col-2 saveBtn." ).click(function() {
-//   localStorage.setItem(".row", $(".row".valueOf )
-// )}
-
+// click button and save to local storage
+   var storageList = document.querySelector(".col-8");
+   var textInput = document.querySelector(".col-8");
+   var button = $(".col-2 saveBtn")
+   
+   storageInput.addEventListeneer('textarea', data =>{
+      console.log(data.target.value)
+      textInput.textContent = data.target.value
+   })
 // to retrieve from local storage
+localStorage.getItem
 // localStorage.getItem(".row", $(".row".valueOf ));
 
 
